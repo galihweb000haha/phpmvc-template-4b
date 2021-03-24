0dfdd8e9d4907fad613e92938e97b3862eafb6bf
yu jin <galih11120@gmail.com>
2021-03-24 22:20:32 +0700

    update template

---
+++ Public/index.php
@@ -0,0 +1,7 @@
+<?php
+define('ROOT', str_replace("Public/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+// $root = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+// $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+// $root = rtrim($root, '/');
+// define('BASEURL', $root);
+require(ROOT . 'Config/intip.php');
